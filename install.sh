d6670ac573d26f8b0512fb8f6c29f87089a7f58b
CerealBoy <allan.shone@yahoo.com>
2016-03-28 16:06:37 +1100

    Bring in direnv and add to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-git submodule init && git submodule update
+# bring in any submodule updates
+git submodule init && git submodule update --remote --recursive
 
+# install direnv, probably need sudo...
+cd direnv
+make direnv
+sudo cp direnv /usr/local/bin/direnv
+sudo chmod a+x /usr/local/bin/direnv
+cd ..
+
+# remove the old files
 rm -rf ~/.vim ~/.vimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.profile ~/.screenrc ~/.atom
 
+# symlink them in
 ln -s "$PWD/.vim" ~/.vim
 ln -s "$PWD/.vim/vimrc" ~/.vimrc
 ln -s "$PWD/.bashrc" ~/.bashrc
@@ -13,3 +23,6 @@ ln -s "$PWD/.gitconfig" ~/.gitconfig
 ln -s "$PWD/.profile" ~/.profile
 ln -s "$PWD/.screenrc" ~/.screenrc
 ln -s "$PWD/.atom" ~/.atom
+
+# use the profile pls
+. ~/.bash_profile
